fb076dc286f7dc4eb533b981ac5b6dfd472da916
Paul <paul.roberts@C02XT4NCJGH5.fios-router.home>
2020-01-06 14:37:14 -0500

    HostListener to listen to events

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -7,9 +7,17 @@ export class BetterHighlightDirective implements OnInit {
   //renderer is better to used for DOM manipulation because the element not exist. Thus if you used grab
   // the element directly, it might throw an null pointer error
   ngOnInit(): void {
-    this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
+    // this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
   }
 
   constructor(private elRef: ElementRef,private renderer: Renderer2) { }
+
+  @HostListener('mouseenter') mouseover(eventData: Event){
+    this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent');
+  }
 
 }
